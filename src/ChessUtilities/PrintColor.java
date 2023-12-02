c33bacf05d8a87de3e5694705416e2c38ffb7cdb
JDub323 <jwhalen323@gmail.com>
2023-12-02 12:36:38 -0600

    2.0

---
+++ src/ChessUtilities/PrintColor.java
@@ -0,0 +1,8 @@
+package ChessUtilities;
+
+public class PrintColor {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+}
